8f227af20e87282f3749aba3b11dfe0f74e97a12
Fernando Yedra <A2183920@correo.uia.mx>
2021-03-30 13:29:18 -0600

    guardar numeros en un arreglo

--- ordenar.c
+++ ordenar.c
@@ -10,21 +10,26 @@
 #define MIN 0
 
 int main(int argc, char *argv[]){
-	int n, i, numero;
+	int n, i;
 	FILE *fp;
 
 	printf("¿Cuántos números desea generar?\n");
 	printf("n = ");
 	scanf(" %i", &n);
+	int numeros[n];
 	fp = fopen("aleatrios.txt", "w");
 	if(fp == NULL){
 		printf("ERROR: Archivo (aleatorios.txt) no disponible\n");
 		exit(1);
 	}
 	srand(time(0));
+	printf("+++++Lista generada++++\n");
 	for(i = 0; i < n; i++){
-		fprintf(fp, "%i\n", rand() % (MAX + 1 - MIN) + MIN);
+		numeros[i] = rand() % (MAX + 1 - MIN) + MIN;
+		printf("%i\n", numeros[i]);
+		fprintf(fp, "%i\n", numeros[i]);
 	}
+	printf("+++++++++++++++++++++++\n");
 	fclose(fp);
 	printf("Archivo de numeros aleatorios escrito\n");
 	//TODO El resto del progema
